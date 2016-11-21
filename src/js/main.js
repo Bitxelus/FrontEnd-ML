270319dfebddd66a7bdf46777d3d0ff95b7dafc9
Bitxelus <dmbruruguay@gmail.com>
2016-11-21 03:17:07 -0300

    Add files via upload
    Versión 1

---
+++ src/js/main.js
@@ -0,0 +1,21 @@
+//Slider de productos relacionados
+var carousel = new ch.Carousel(ch('.demo-carousel')[0], {});
+
+//Tabs de descripcion y comentarios
+var tabs = new ch.Tabs(ch(".descripcion-tabs")[0]);
+tabs.show(2);
+
+//Valores cantidad y pagos
+var cantidadDeProductos = 9;
+var cantidadTotalTexto = "";
+for (i=1; i <= cantidadDeProductos; i++){
+  cantidadTotalTexto = cantidadTotalTexto + '<option value="' + i + '">' + i + '</option>';
+}
+document.getElementById('cantidad').innerHTML = cantidadTotalTexto;
+
+var cantidadDePagos = 12;
+var cantidadTotalTexto = "";
+for (i=1; i <= cantidadDePagos; i++){
+  cantidadTotalTexto = cantidadTotalTexto + '<option value="' + i + '">' + i + '</option>';
+}
+document.getElementById('pagos').innerHTML = cantidadTotalTexto;
